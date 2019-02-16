d94d78df5071b191cdb6ae4dadc573004d748c5c
halloffamezwx <768473189@qq.com>
2019-02-16 15:53:09 +0800

    优雅客户端支持连接池

--- thriftjsoa/src/main/java/com/halloffame/thriftjsoa/common/CommonClient.java
+++ thriftjsoa/src/main/java/com/halloffame/thriftjsoa/common/CommonClient.java
@@ -2,12 +2,10 @@ package com.halloffame.thriftjsoa.common;
 
 import com.halloffame.thriftjsoa.base.ThirftJsoaException;
 import com.halloffame.thriftjsoa.base.ThirftJsoaProtocol;
+import com.halloffame.thriftjsoa.config.ClientClassConfig;
 import com.halloffame.thriftjsoa.config.ClientConfig;
 import org.apache.thrift.TServiceClient;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TCompactProtocol;
-import org.apache.thrift.protocol.TJSONProtocol;
-import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.*;
 import org.apache.thrift.transport.*;
 
 import java.lang.reflect.Constructor;
@@ -17,14 +15,14 @@ import java.util.Map;
  * 客户端工具类
  */
 public class CommonClient {
-    public static final ThreadLocal<Map<Class<? extends TServiceClient>, TServiceClient>> tServiceClientMapThreadLocal = new ThreadLocal<>();
-    public static final String HTTP_URL_TEMPLATE = "http://%s:%s/service";
+    public static final ThreadLocal<Map<Class<? extends TServiceClient>, TServiceClient>> T_SERVICE_CLIENT_MAP_THREAD_LOCAL = new ThreadLocal<>(); //客户端上下文变量
+    public static final String HTTP_URL_TEMPLATE = "http://%s:%s/service"; //客户端transport_type为http的url模板，第一个通配符是host，第二个通配符是port
 
     /**
      * 从ThreadLocal里取得客户端
      */
     public static <T extends TServiceClient> T getClient(Class<T> clazz) {
-        Map<Class<? extends TServiceClient>, TServiceClient> tServiceClientMap = tServiceClientMapThreadLocal.get();
+        Map<Class<? extends TServiceClient>, TServiceClient> tServiceClientMap = T_SERVICE_CLIENT_MAP_THREAD_LOCAL.get();
         TServiceClient tServiceClient = tServiceClientMap.get(clazz);
         return (T)tServiceClient;
     }
@@ -32,74 +30,103 @@ public class CommonClient {
     /**
      * 根据配置创建客户端
      */
-    public static <T extends TServiceClient> T createClient(Class<T> clazz, ClientConfig clientConfig) throws Exception {
-        String protocol_type = clientConfig.getProtocolType(); //需要和服务端的一致才能正常通信
-        String transport_type = clientConfig.getTransportType(); //需要和服务端的一致才能正常通信
-        boolean ssl = clientConfig.isSsl(); //传输是否加密
-        int socketTimeout = clientConfig.getSocketTimeout(); //读超时时间
-        String host = clientConfig.getHost(); //服务主机
-        int port = clientConfig.getPort(); //服务端口
+    public static <T extends TServiceClient> T createClient(Class<T> clazz, ClientConfig clientConfig,
+                                                            Map<Class<? extends TServiceClient>, TProtocol> tServiceClientProtocolMap,
+                                                            Map<TProtocol, ConnectionPoolFactory> tProtocolPoolMap) throws Exception {
+        TProtocol tProtocol = null;
+        ConnectionPoolFactory poolFactory = clientConfig.getPoolFactory(); //连接池，没有配置就为null
+        String serviceName = null; //TMultiplexedProtocol的SERVICE_NAME
 
-        //检查传入的变量值是否正确
-        if (protocol_type.equals(ProtocolType.BINARY)) {
-        } else if (protocol_type.equals(ProtocolType.COMPACT)) {
-        } else if (protocol_type.equals(ProtocolType.JSON)) {
-        } else {
-            throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "Unknown protocol type! " + protocol_type);
-        }
-
-        if (transport_type.equals(TransportType.BUFFERED)) {
-        } else if (transport_type.equals(TransportType.FRAMED)) {
-        } else if (transport_type.equals(TransportType.FASTFRAMED)) {
-        } else if (transport_type.equals(TransportType.HTTP)) {
-        } else {
-            throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "Unknown transport type! " + transport_type);
-        }
-
-        if (transport_type.equals(TransportType.HTTP) && ssl == true) { //不支持https
-            throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "SSL is not supported over http.");
-        }
-
-        TTransport transport; //指定的通信方式
-
-        if (transport_type.equals(TransportType.HTTP)) {
-            String url = String.format(HTTP_URL_TEMPLATE, host, port);
-            transport = new THttpClient(url);
-        } else {
-            TSocket socket;
-            if (ssl == true) {
-                socket = TSSLTransportFactory.getClientSocket(host, port, 0);
+        for (ClientClassConfig clientClassConfig : clientConfig.getClientClassConfigs()) {
+            if (clazz == clientClassConfig.getClazz()) {
+                serviceName = clientClassConfig.getName();
             } else {
-                socket = new TSocket(host, port);
+                tProtocol = tServiceClientProtocolMap.get(clientClassConfig.getClazz());
+            }
+        }
+
+        if (tProtocol == null) {
+            if (poolFactory != null) {
+                tProtocol = poolFactory.getConnection();
+            }
+        }
+
+        if (tProtocol == null) {
+            String protocol_type = clientConfig.getProtocolType(); //需要和服务端的一致才能正常通信
+            String transport_type = clientConfig.getTransportType(); //需要和服务端的一致才能正常通信
+            boolean ssl = clientConfig.isSsl(); //传输是否加密
+            int socketTimeout = clientConfig.getSocketTimeout(); //读超时时间
+            String host = clientConfig.getHost(); //服务主机
+            int port = clientConfig.getPort(); //服务端口
+
+            //检查传入的变量值是否正确
+            if (protocol_type.equals(ProtocolType.BINARY)) {
+            } else if (protocol_type.equals(ProtocolType.COMPACT)) {
+            } else if (protocol_type.equals(ProtocolType.JSON)) {
+            } else {
+                throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "Unknown protocol type! " + protocol_type);
             }
 
-            socket.setTimeout(socketTimeout);
-
-            transport = socket;
             if (transport_type.equals(TransportType.BUFFERED)) {
             } else if (transport_type.equals(TransportType.FRAMED)) {
-                transport = new TFramedTransport(transport);
             } else if (transport_type.equals(TransportType.FASTFRAMED)) {
-                transport = new TFastFramedTransport(transport);
+            } else if (transport_type.equals(TransportType.HTTP)) {
+            } else {
+                throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "Unknown transport type! " + transport_type);
+            }
+
+            if (transport_type.equals(TransportType.HTTP) && ssl == true) { //不支持https
+                throw new ThirftJsoaException(MsgCode.THRIFTJSOA_EXCEPTION, "SSL is not supported over http.");
+            }
+
+            TTransport transport; //指定的通信方式
+
+            if (transport_type.equals(TransportType.HTTP)) {
+                String url = String.format(HTTP_URL_TEMPLATE, host, port);
+                transport = new THttpClient(url);
+            } else {
+                TSocket socket;
+                if (ssl == true) {
+                    socket = TSSLTransportFactory.getClientSocket(host, port, 0);
+                } else {
+                    socket = new TSocket(host, port);
+                }
+
+                socket.setTimeout(socketTimeout);
+
+                transport = socket;
+                if (transport_type.equals(TransportType.BUFFERED)) {
+                } else if (transport_type.equals(TransportType.FRAMED)) {
+                    transport = new TFramedTransport(transport);
+                } else if (transport_type.equals(TransportType.FASTFRAMED)) {
+                    transport = new TFastFramedTransport(transport);
+                }
+            }
+
+            if (protocol_type.equals(ProtocolType.JSON)) {
+                tProtocol = new TJSONProtocol(transport);
+            } else if (protocol_type.equals(ProtocolType.COMPACT)) {
+                tProtocol = new TCompactProtocol(transport);
+            } else {
+                tProtocol = new TBinaryProtocol(transport);
+            }
+            tProtocol = new ThirftJsoaProtocol(tProtocol);
+
+            if (transport.isOpen() == false) {
+                transport.open();
             }
         }
 
-        TProtocol tProtocol;
-        if (protocol_type.equals(ProtocolType.JSON)) {
-            tProtocol = new TJSONProtocol(transport);
-        } else if (protocol_type.equals(ProtocolType.COMPACT)) {
-            tProtocol = new TCompactProtocol(transport);
-        } else {
-            tProtocol = new TBinaryProtocol(transport);
-        }
-        tProtocol = new ThirftJsoaProtocol(tProtocol);
+        tServiceClientProtocolMap.put(clazz, tProtocol); //如果是TMultiplexedProtocol，多个客户端对应同一个TProtocol
+        tProtocolPoolMap.put(tProtocol, poolFactory); //最后需要释放资源的TProtocol和配置的连接池（可能为null代表没有配置）
 
-        if (transport.isOpen() == false) {
-            transport.open();
+        TProtocol finalProtocol = tProtocol;
+        if (clientConfig.getClientClassConfigs().size() > 1) {
+            finalProtocol = new TMultiplexedProtocol(tProtocol, serviceName);
         }
 
         Constructor<T> constructor = clazz.getConstructor(TProtocol.class);
-        T t = constructor.newInstance(tProtocol);
+        T t = constructor.newInstance(finalProtocol);
 
         return t;
     }
--- thriftjsoa/src/main/java/com/halloffame/thriftjsoa/config/ClientConfig.java
+++ thriftjsoa/src/main/java/com/halloffame/thriftjsoa/config/ClientConfig.java
@@ -1,6 +1,9 @@
 package com.halloffame.thriftjsoa.config;
 
-import org.apache.thrift.TServiceClient;
+import com.halloffame.thriftjsoa.common.ConnectionPoolFactory;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import java.util.List;
 
 /**
  * 客户端配置
@@ -9,16 +12,18 @@ public class ClientConfig extends BaseConfig {
 	private int socketTimeout = 3000; //socket读超时时间，默认3000ms
 	private String host; //服务主机
 	private int port; //服务端口
-	private Class<? extends TServiceClient> clazz; //客户端的class
+	private List<ClientClassConfig> clientClassConfigs; //客户端的class list
+	private GenericObjectPoolConfig poolConfig; //连接池配置
+	private ConnectionPoolFactory poolFactory; //连接池
 
-	public ClientConfig(String host, int port, Class<? extends TServiceClient> clazz) {
+	public ClientConfig(String host, int port, List<ClientClassConfig> clientClassConfigs) {
 		this.host = host;
 		this.port = port;
-		this.clazz = clazz;
+		this.clientClassConfigs = clientClassConfigs;
 	}
 
-	public Class<? extends TServiceClient> getClazz() {
-		return clazz;
+	public List<ClientClassConfig> getClientClassConfigs() {
+		return clientClassConfigs;
 	}
 
 	public String getHost() {
@@ -35,5 +40,19 @@ public class ClientConfig extends BaseConfig {
 
 	public void setSocketTimeout(int socketTimeout) {
 		this.socketTimeout = socketTimeout;
+	}
+
+	public GenericObjectPoolConfig getPoolConfig() {
+		return poolConfig;
+	}
+
+	public void setPoolConfig(GenericObjectPoolConfig poolConfig) {
+		this.poolConfig = poolConfig;
+		poolFactory = new ConnectionPoolFactory(poolConfig, host, port,
+				socketTimeout, isSsl(), getTransportType(), getProtocolType());
+	}
+
+	public ConnectionPoolFactory getPoolFactory() {
+		return poolFactory;
 	}
 }
